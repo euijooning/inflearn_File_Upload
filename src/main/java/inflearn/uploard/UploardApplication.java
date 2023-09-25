f23740e0a3acf0c8e658811a0ccc88a271fef1e0
EUIJOON <euijoonism@gmail.com>
2023-09-25 13:23:53 +0900

    file_uploard first commit

---
+++ src/main/java/inflearn/uploard/UploardApplication.java
@@ -0,0 +1,13 @@
+package inflearn.uploard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/inflearn/uploard/UploardApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.uploard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
